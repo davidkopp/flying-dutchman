71a1176b2f76af414e1423df191f100996c00d2d
David Kopp <mail@davidkopp.de>
2022-02-24 09:44:55 +0100

    Refactoring language switching

--- controllers/LanguageController.js
+++ controllers/LanguageController.js
@@ -22,14 +22,95 @@
         updateView();
     }
 
-    /** Updates the view by replacing all text strings. */
+    /**
+     * Extracts the current page name out of the window location, removes the
+     * slash in the front and removes the filetype.
+     *
+     * @returns {string} Current page name
+     */
+    function getCurrentPageName() {
+        let currentPageName = window.location.pathname;
+        if (currentPageName.charAt(0) === "/") {
+            currentPageName = currentPageName.slice(1);
+        }
+        if (currentPageName.includes(".html")) {
+            currentPageName = currentPageName.slice(
+                0,
+                currentPageName.indexOf(".html")
+            );
+        }
+        return currentPageName;
+    }
+
+    /** Updates the current view by replacing all text strings. */
     function updateView() {
-        $("#welcome-text").text(Dictionary.getString("hello_text"));
-        $("#username-label").text(Dictionary.getString("username"));
-        $("#password-label").text(Dictionary.getString("password"));
-        $("#order").text(Dictionary.getString("order"));
-        $("#pay").text(Dictionary.getString("pay"));
-        $("#login-form-submit").val(Dictionary.getString("login"));
+        updateCommonTextStrings();
+        const currentPageName = getCurrentPageName();
+        switch (currentPageName) {
+            case "index":
+                updateViewIndex();
+                break;
+            case "login":
+                updateViewLogin();
+                break;
+            case "menu":
+                updateViewMenu();
+                break;
+            default:
+                console.log(
+                    `LanguageController.updateView | Page '${currentPageName}' unknown!`
+                );
+                break;
+        }
+    }
+
+    /** Updates text strings that are common through multiple pages. */
+    function updateCommonTextStrings() {
+        $("#language-switcher-text-english").text(
+            Dictionary.getCommonString("language-switcher-text-english")
+        );
+        $("#language-switcher-text-german").text(
+            Dictionary.getCommonString("language-switcher-text-german")
+        );
+        $("#language-switcher-text-portugese").text(
+            Dictionary.getCommonString("language-switcher-text-portugese")
+        );
+    }
+
+    /** Updates the view 'index' by replacing all text strings. */
+    function updateViewIndex() {
+        const page = "index";
+        $("#page-title").text(Dictionary.getPageString(page, "page-title"));
+        $("#caption").text(Dictionary.getPageString(page, "caption"));
+        $("#welcome-text").text(Dictionary.getPageString(page, "hello_text"));
+        $("#link-to-login-page").text(
+            Dictionary.getPageString(page, "link-to-login-page")
+        );
+        $("#link-to-menu-page").text(
+            Dictionary.getPageString(page, "link-to-menu-page")
+        );
+    }
+
+    /** Updates the view 'login' by replacing all text strings. */
+    function updateViewLogin() {
+        const page = "login";
+        $("#page-title").text(Dictionary.getPageString(page, "page-title"));
+        $("#caption").text(Dictionary.getPageString(page, "caption"));
+        $("#username-label").text(Dictionary.getPageString(page, "username"));
+        $("#password-label").text(Dictionary.getPageString(page, "password"));
+        $("#login-form-submit").val(
+            Dictionary.getPageString(page, "login-form-submit")
+        );
+    }
+
+    /** Updates the view 'menu' by replacing all text strings. */
+    function updateViewMenu() {
+        const page = "menu";
+
+        $("#page-title").text(Dictionary.getPageString(page, "page-title"));
+        $("#caption").text(Dictionary.getPageString(page, "caption"));
+        $("#order").text(Dictionary.getPageString(page, "order"));
+        $("#pay").text(Dictionary.getPageString(page, "pay"));
     }
 
     $(document).ready(function () {
--- database/Dictionary.js
+++ database/Dictionary.js
@@ -3,39 +3,116 @@ Dictionary = (function () {
 
     const dict = {
         en: {
-            hello_text: "Welcome to Flying Dutchman",
-            username: "Username",
-            password: "Password",
-            order: "Order",
-            pay: "Pay",
-            login: "Login",
+            // Common text strings
+            _: {
+                "language-switcher-text-english": "English",
+                "language-switcher-text-german": "German",
+                "language-switcher-text-portugese": "Portugese",
+            },
+            // Text strings for the page 'index'
+            index: {
+                "page-title": "Flying Dutchman - English",
+                "caption": "Welcome to Flying Dutchman",
+                "link-to-login-page": "Login",
+                "link-to-menu-page": "Menu",
+            },
+            // Text strings for the page 'menu'
+            menu: {
+                "page-title": "Flying Dutchman - Menu",
+                "caption": "Menu",
+                "order": "Order",
+                "pay": "Pay",
+            },
+            // Text strings for the page 'login'
+            login: {
+                "page-title": "Flying Dutchman - Login",
+                "caption": "Login",
+                "username": "Username",
+                "password": "Password",
+                "login-form-submit": "Login",
+            },
         },
         de: {
-            hello_text: "Willkommen zum Flying Dutchman",
-            username: "Benutzername",
-            password: "Passwort",
-            order: "Bestellung",
-            pay: "Zahlung",
-            login: "Anmelden",
+            // Common text strings
+            _: {
+                "language-switcher-text-english": "Englisch",
+                "language-switcher-text-german": "Deutsch",
+                "language-switcher-text-portugese": "Portugiesisch",
+            },
+            // Text strings for the page 'index'
+            index: {
+                "page-title": "Flying Dutchman - Englisch",
+                "caption": "Willkommen zum Flying Dutchman",
+                "link-to-login-page": "Anmelden",
+                "link-to-menu-page": "Menü",
+            },
+            // Text strings for the page 'menu'
+            menu: {
+                "page-title": "Flying Dutchman - Menü",
+                "caption": "Menü",
+                "order": "Bestellung",
+                "pay": "Zahlung",
+            },
+            // Text strings for the page 'login'
+            login: {
+                "page-title": "Flying Dutchman - Anmeldung",
+                "caption": "Anmeldung",
+                "username": "Benutzername",
+                "password": "Passwort",
+                "login-form-submit": "Anmelden",
+            },
         },
         pt: {
-            hello_text: "Bemvindo ao Flying Dutchman",
-            username: "",
-            password: "",
-            order: "",
-            pay: "",
-            login: "",
+            // Common text strings
+            _: {
+                "language-switcher-text-english": "Englisch",
+                "language-switcher-text-german": "Deutsch",
+                "language-switcher-text-portugese": "Portugiesisch",
+            },
+            // Text strings for the page 'index'
+            index: {
+                "page-title": "",
+                "caption": "Bemvindo ao Flying Dutchman",
+                "link-to-login-page": "",
+                "link-to-menu-page": "",
+            },
+            // Text strings for the page 'menu'
+            menu: {
+                "page-title": "",
+                "caption": "",
+                "order": "",
+                "pay": "",
+            },
+            // Text strings for the page 'login'
+            login: {
+                "page-title": "",
+                "caption": "",
+                "username": "",
+                "password": "",
+                "login-form-submit": "",
+            },
         },
     };
 
     /**
-     * Get the appropriate string for a given key.
+     * Get the appropriate common string for a given key.
      *
      * @param {string} key The key.
      * @returns {string} The text string.
      */
-    function getString(key) {
-        return dict[language][key];
+    function getCommonString(key) {
+        return dict[language]["_"][key];
+    }
+
+    /**
+     * Get the appropriate string for a given page and key.
+     *
+     * @param {string} page The page.
+     * @param {string} key The key.
+     * @returns {string} The text string.
+     */
+    function getPageString(page, key) {
+        return dict[language][page][key];
     }
 
     /**
@@ -57,7 +134,8 @@ Dictionary = (function () {
     }
 
     return {
-        getString: getString,
+        getCommonString: getCommonString,
+        getPageString: getPageString,
         getCurrentLanguage: getCurrentLanguage,
         setLanguage: setLanguage,
     };
