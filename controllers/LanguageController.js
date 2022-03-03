9a1f9c1753df14e9cd618180fb1cc1bf1e48895a
David Kopp <mail@davidkopp.de>
2022-03-03 08:50:23 +0100

    Language switching: Use SessionStorage + Refactoring

--- controllers/LanguageController.js
+++ controllers/LanguageController.js
@@ -5,11 +5,17 @@
  *
  * Author: David Kopp
  * -----
- * Last Modified: Thursday, 24th February 2022
+ * Last Modified: Thursday, 3rd March 2022
  * Modified By: David Kopp (mail@davidkopp.de>)
  */
 
 (function ($, exports) {
+    /** Initialize the language and update the view when the DOM is ready. */
+    $(document).ready(function () {
+        init();
+        updateView();
+    });
+
     /**
      * Changes the language for the whole system and updates the view automatically.
      *
@@ -18,13 +24,13 @@
     function changeLang(new_lang) {
         switch (new_lang) {
             case "en":
-                Dictionary.setLanguage("en");
+                setLanguage("en");
                 break;
             case "de":
-                Dictionary.setLanguage("de");
+                setLanguage("de");
                 break;
             case "pt":
-                Dictionary.setLanguage("pt");
+                setLanguage("pt");
                 break;
             default:
                 console.error(`Language ${new_lang} not known.`);
@@ -54,6 +60,16 @@
         return currentPath;
     }
 
+    /** Initializes the language. If their is no language set, use the default language. */
+    function init() {
+        if (!getCurrentLanguage()) {
+            console.log(
+                `LanguageController.init | There is no language definied yet. Use the default language '${Constants.DEFAULT_LANGUAGE}'.`
+            );
+            setLanguage(Constants.DEFAULT_LANGUAGE);
+        }
+    }
+
     /**
      * Updates the current view by replacing all text strings. Per default it
      * uses the html element property `text` to replace the text. However, not
@@ -66,16 +82,76 @@
             let langKey = $(element).data("lang").trim();
             if (langKey.startsWith("[value]")) {
                 langKey = langKey.slice(7, langKey.length);
-                $(element).val(Dictionary.getString(langKey, currentPath));
+                $(element).val(getString(langKey, currentPath));
             } else {
-                $(element).text(Dictionary.getString(langKey, currentPath));
+                $(element).text(getString(langKey, currentPath));
             }
         });
     }
 
-    $(document).ready(function () {
-        updateView();
-    });
+    /**
+     * Get the current language from the sessionStorage.
+     *
+     * @returns {string} The current language as country code.
+     */
+    function getCurrentLanguage() {
+        return sessionStorage.getItem(Constants.SESSION_STORAGE_LANGUAGE_KEY);
+    }
+
+    /**
+     * Save the new language in the sessionStorage
+     *
+     * @param {string} newLang The new language as country code.
+     */
+    function setLanguage(newLang) {
+        sessionStorage.setItem(Constants.SESSION_STORAGE_LANGUAGE_KEY, newLang);
+    }
+
+    /**
+     * Get the appropriate string for a key and an optional page. If no page is
+     * given the key is supposed to be a key for a common string.
+     *
+     * @param {string} key The key.
+     * @param {string} [page] The page. If not set the key is supposed to be a
+     *   key for a common string.
+     * @returns {string} The text string.
+     */
+    function getString(key, page) {
+        let result = undefined;
+        const currentLanguage = getCurrentLanguage();
+        if (page) {
+            // Get the value for the key and the provided page.
+            let pageDict = Dictionary[currentLanguage][page];
+            if (pageDict) {
+                result = pageDict[key];
+                if (!result) {
+                    // The key does not exist for the given page.
+                    // Check if the key is known as a common key.
+                    result = Dictionary[currentLanguage]["_"][key];
+                    if (!result) {
+                        console.log(
+                            `LanguageController.getString | Language key '${key}' is not set for language '${currentLanguage}' and page '${page}'.`
+                        );
+                    }
+                }
+            } else {
+                console.log(
+                    `LanguageController.getString | Page '${page}' is unknown in the Dictionary.`
+                );
+            }
+        }
+
+        if (!result) {
+            // Try to get the value for the common key.
+            result = Dictionary[currentLanguage]["_"][key];
+            if (!result) {
+                console.log(
+                    `LanguageController.getString | Common language key '${key}' is not set for language '${currentLanguage}'.`
+                );
+            }
+        }
+        return result;
+    }
 
     exports.changeLang = changeLang;
 })(jQuery, window);
--- database/Dictionary.js
+++ database/Dictionary.js
@@ -1,18 +1,19 @@
 /*
  * File: Dictionary.js
  *
- * Dictionary that is used for the localization of the application.
- * The file also includes some constants.
+ * This file includes some global constants and the dictionary for the localization of the application.
  *
  * Author: David Kopp
  * -----
- * Last Modified: Tuesday, 1st March 2022
+ * Last Modified: Thursday, 3rd March 2022
  * Modified By: David Kopp (mail@davidkopp.de>)
  */
 /* eslint-disable no-redeclare */
 /* eslint-disable no-unused-vars */
 
 const Constants = {
+    DEFAULT_LANGUAGE: "en",
+
     PAGE_HOME: "index",
     PAGE_LOGIN: "login",
     PAGE_MENU: "menu",
@@ -37,182 +38,111 @@ const Constants = {
     },
 
     LOW_STOCK_NUMBER: 5,
+
+    SESSION_STORAGE_LANGUAGE_KEY: "language",
 };
 
-const Dictionary = (function () {
-    let language = "en";
-
-    const dict = {
-        // ENGLISH
-        en: {
-            // Common text strings
-            _: {
-                "language-switcher-text-english": "English",
-                "language-switcher-text-german": "German",
-                "language-switcher-text-portuguese": "Portuguese",
-            },
-            // Text strings for the page 'index'
-            index: {
-                "page-title": "Flying Dutchman - English",
-                "caption": "Welcome to Flying Dutchman",
-                "link-to-login-page": "Login",
-                "link-to-menu-page": "Menu",
-            },
-            // Text strings for the page 'menu'
-            menu: {
-                "page-title": "Flying Dutchman - Menu",
-                "caption": "Menu",
-                "order": "Order",
-                "pay": "Pay",
-            },
-            // Text strings for the page 'login'
-            login: {
-                "page-title": "Flying Dutchman - Login",
-                "caption": "Login",
-                "username-label": "Username",
-                "password-label": "Password",
-                "login-form-submit": "Login",
-            },
-            SpecRunner: {
-                "unit-test": "english",
-            },
+const Dictionary = {
+    // ENGLISH
+    en: {
+        // Common text strings (relevant for multiple pages)
+        _: {
+            "language-switcher-text-english": "English",
+            "language-switcher-text-german": "German",
+            "language-switcher-text-portuguese": "Portuguese",
+            "link-to-login-page": "Login",
         },
-        // GERMAN
-        de: {
-            // Common text strings
-            _: {
-                "language-switcher-text-english": "Englisch",
-                "language-switcher-text-german": "Deutsch",
-                "language-switcher-text-portuguese": "Portugiesisch",
-            },
-            // Text strings for the page 'index'
-            index: {
-                "page-title": "Flying Dutchman - Englisch",
-                "caption": "Willkommen zum Flying Dutchman",
-                "link-to-login-page": "Anmelden",
-                "link-to-menu-page": "Menü",
-            },
-            // Text strings for the page 'menu'
-            menu: {
-                "page-title": "Flying Dutchman - Menü",
-                "caption": "Menü",
-                "order": "Bestellung",
-                "pay": "Zahlung",
-            },
-            // Text strings for the page 'login'
-            login: {
-                "page-title": "Flying Dutchman - Anmeldung",
-                "caption": "Anmeldung",
-                "username-label": "Benutzername",
-                "password-label": "Passwort",
-                "login-form-submit": "Anmelden",
-            },
-            SpecRunner: {
-                "unit-test": "german",
-            },
+        // Text strings for the page 'index'
+        index: {
+            "page-title": "Flying Dutchman - English",
+            "caption": "Welcome to Flying Dutchman",
+            "link-to-menu-page": "Menu",
         },
-        // Portuguese
-        pt: {
-            // Common text strings
-            _: {
-                "language-switcher-text-english": "Englisch",
-                "language-switcher-text-german": "Deutsch",
-                "language-switcher-text-portuguese": "Portugiesisch",
-            },
-            // Text strings for the page 'index'
-            index: {
-                "page-title": "",
-                "caption": "Bemvindo ao Flying Dutchman",
-                "link-to-login-page": "",
-                "link-to-menu-page": "",
-            },
-            // Text strings for the page 'menu'
-            menu: {
-                "page-title": "",
-                "caption": "",
-                "order": "",
-                "pay": "",
-            },
-            // Text strings for the page 'login'
-            login: {
-                "page-title": "",
-                "caption": "",
-                "username-label": "",
-                "password-label": "",
-                "login-form-submit": "",
-            },
-            SpecRunner: {
-                "unit-test": "portuguese",
-            },
+        // Text strings for the page 'menu'
+        menu: {
+            "page-title": "Flying Dutchman - Menu",
+            "caption": "Menu",
+            "order": "Order",
+            "pay": "Pay",
         },
-    };
-
-    /**
-     * Get the appropriate string for a key and an optional page. If no page is
-     * given the key is supposed to be a key for a common string.
-     *
-     * @param {string} key The key.
-     * @param {string} [page] The page. If not set the key is supposed to be a
-     *   key for a common string.
-     * @returns {string} The text string.
-     */
-    function getString(key, page) {
-        let result = undefined;
-        if (page) {
-            // Get the value for the key and the provided page.
-            let pageDict = dict[language][page];
-            if (pageDict) {
-                result = pageDict[key];
-                if (!result) {
-                    // The key does not exist for the given page.
-                    // Check if the key is known as a common key.
-                    result = dict[language]["_"][key];
-                    if (!result) {
-                        console.log(
-                            `Dictionary.getString | Language key '${key}' is not set for language '${language}' and page '${page}'.`
-                        );
-                    }
-                }
-            } else {
-                console.log(
-                    `Dictionary.getString | Page '${page}' is unknown in the Dictionary.`
-                );
-            }
-        }
-
-        if (!result) {
-            // Try to get the value for the common key.
-            result = dict[language]["_"][key];
-            if (!result) {
-                console.log(
-                    `Dictionary.getString | Common language key '${key}' is not set for language '${language}'.`
-                );
-            }
-        }
-        return result;
-    }
-
-    /**
-     * Get the current language.
-     *
-     * @returns {string} The current language as country code.
-     */
-    function getCurrentLanguage() {
-        return language;
-    }
-
-    /**
-     * Set a new language.
-     *
-     * @param {string} newLang The new language as country code.
-     */
-    function setLanguage(newLang) {
-        language = newLang;
-    }
-
-    return {
-        getString: getString,
-        getCurrentLanguage: getCurrentLanguage,
-        setLanguage: setLanguage,
-    };
-})();
+        // Text strings for the page 'login'
+        login: {
+            "page-title": "Flying Dutchman - Login",
+            "caption": "Login",
+            "username-label": "Username",
+            "password-label": "Password",
+            "login-form-submit": "Login",
+        },
+        SpecRunner: {
+            "unit-test": "english",
+        },
+    },
+    // GERMAN
+    de: {
+        // Common text strings (relevant for multiple pages)
+        _: {
+            "language-switcher-text-english": "Englisch",
+            "language-switcher-text-german": "Deutsch",
+            "language-switcher-text-portuguese": "Portugiesisch",
+            "link-to-login-page": "Anmelden",
+        },
+        // Text strings for the page 'index'
+        index: {
+            "page-title": "Flying Dutchman - Englisch",
+            "caption": "Willkommen zum Flying Dutchman",
+            "link-to-menu-page": "Menü",
+        },
+        // Text strings for the page 'menu'
+        menu: {
+            "page-title": "Flying Dutchman - Menü",
+            "caption": "Menü",
+            "order": "Bestellung",
+            "pay": "Zahlung",
+        },
+        // Text strings for the page 'login'
+        login: {
+            "page-title": "Flying Dutchman - Anmeldung",
+            "caption": "Anmeldung",
+            "username-label": "Benutzername",
+            "password-label": "Passwort",
+            "login-form-submit": "Anmelden",
+        },
+        SpecRunner: {
+            "unit-test": "german",
+        },
+    },
+    // Portuguese
+    pt: {
+        // Common text strings (relevant for multiple pages)
+        _: {
+            "language-switcher-text-english": "Englisch",
+            "language-switcher-text-german": "Deutsch",
+            "language-switcher-text-portuguese": "Portugiesisch",
+            "link-to-login-page": "",
+        },
+        // Text strings for the page 'index'
+        index: {
+            "page-title": "",
+            "caption": "Bemvindo ao Flying Dutchman",
+            "link-to-menu-page": "",
+        },
+        // Text strings for the page 'menu'
+        menu: {
+            "page-title": "",
+            "caption": "",
+            "order": "",
+            "pay": "",
+        },
+        // Text strings for the page 'login'
+        login: {
+            "page-title": "",
+            "caption": "",
+            "username-label": "",
+            "password-label": "",
+            "login-form-submit": "",
+        },
+        SpecRunner: {
+            "unit-test": "portuguese",
+        },
+    },
+};
